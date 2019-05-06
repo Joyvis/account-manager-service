d54eaa69c0bc7e34a9a72d5fa89e189ff16bc0a1
Joyvis Santana <jjs276@gmail.com>
2019-05-06 06:15:06 -0300

    routes and actions to transfers, errors serializing

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -1,2 +1,17 @@
 class TransfersController < ApplicationController
+    def create
+    transfer = Transfer.new(transfer_params)
+    if transfer.save
+      render_success transfer
+    else
+      render_error transfer
+    end
+  end
+
+  private
+
+  def transfer_params
+    params.require(:transfer).permit(:source_account_id,
+                                     :destination_account_id, :amount)
+  end
 end
--- spec/controllers/transfers_controller_spec.rb
+++ spec/controllers/transfers_controller_spec.rb
@@ -1,5 +1,27 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe TransfersController, type: :controller do
+  describe 'creating a transfer' do
+    before do
+      post :create, params: { transfer: transfer_params }
+    end
 
+    context 'with valid params' do
+      let(:transfer_params) { attributes_for(:transfer) }
+
+      it 'transfer created' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'with invalid params' do
+      let(:transfer_params) { { amount: 12 } }
+
+      it 'transfer not created' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
 end
